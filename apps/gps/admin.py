cc983dac1acbb43a02bf645dfbd72688ab63da8b
Tomasz Brzezina <tomasz@brzezina.pl>
2025-12-30 12:29:06 +0100

    geom remove

--- apps/gps/admin.py
+++ apps/gps/admin.py
@@ -2,28 +2,26 @@
 Django Admin Configuration for GPS Application
 """
 from django.contrib import admin
-from django.contrib.gis.admin import OSMGeoAdmin
 from .models import GpsData
 
 
 @admin.register(GpsData)
-class GpsDataAdmin(OSMGeoAdmin):
+class GpsDataAdmin(admin.ModelAdmin):
     """
-    Admin interface for GPS data with map display
+    Admin interface for GPS data
     """
     list_display = ['id', 'timestamp', 'mac', 'latitude', 'longitude', 
                     'speed_kmh', 'num_satellites', 'quality']
     list_filter = ['quality', 'timestamp']
     search_fields = ['mac']
     date_hierarchy = 'timestamp'
-    readonly_fields = ['geom']
     
     fieldsets = (
         ('Basic Info', {
             'fields': ('timestamp', 'mac')
         }),
         ('Location', {
-            'fields': ('latitude', 'longitude', 'altitude', 'geom')
+            'fields': ('latitude', 'longitude', 'altitude')
         }),
         ('GPS Quality', {
             'fields': ('quality', 'num_satellites', 'hdop')
@@ -32,8 +30,3 @@ class GpsDataAdmin(OSMGeoAdmin):
             'fields': ('speed_kmh', 'course')
         }),
     )
-    
-    # Display map with OpenStreetMap
-    default_center_longitude = 18.9659
-    default_center_latitude = 50.2585
-    default_zoom = 15
