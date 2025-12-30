c44928420716f528b2574079d9eb8f1e6c03d454
Tomasz Brzezina <tomasz@brzezina.pl>
2025-12-30 11:47:34 +0100

    remove useless

--- core/settings.py
+++ core/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'crispy_forms',
-    'pluralize_pl',
 
     'apps.gps',
 ]
@@ -148,8 +146,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-AUTH_USER_MODEL = 'people.Person'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
